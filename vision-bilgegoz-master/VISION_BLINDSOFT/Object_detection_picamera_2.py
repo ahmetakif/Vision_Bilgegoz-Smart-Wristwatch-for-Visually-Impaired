d1c807dadc69c3051b893022c925ed517a8c84bb
Ahmet Akif Kaya <ahmetakifkaya11@gmail.com>
2020-02-23 22:59:48 +0300

    Update Object_detection_picamera_2.py

--- vision-bilgegoz-master/VISION_BLINDSOFT/Object_detection_picamera_2.py
+++ vision-bilgegoz-master/VISION_BLINDSOFT/Object_detection_picamera_2.py
@@ -1,13 +1,13 @@
-######## Picamera Object Detection Using Tensorflow Classifier #########
+######## USB Camera Object Detection Using Tensorflow Classifier for VISION-Bilgegoz #########
 #
 # Author: Ahmet Akif Kaya
 # Date: 4/15/18
 # Description:
 # This program uses a TensorFlow classifier to perform object detection.
-# It loads the classifier uses it to perform object detection on a Picamera feed.
+# For detecting the objects in the frame of the camera to be said vocally to the blind user.
+# It loads the classifier uses it to perform object detection on a USB Camera feed.
 # It draws boxes and scores around the objects of interest in each frame from
-# the Picamera. It also can be used with a webcam by adding "--usbcam"
-# when executing this script from the terminal.
+# the USB Camera. It also can be used with a Picamera.
 
 ## Some of the code is copied from EjdeElectronics Tensorflow on Raspiberry Pi Library at
 ## https://github.com/EdjeElectronics/TensorFlow-Object-Detection-on-the-Raspberry-Pi
@@ -17,9 +17,6 @@
 
 ## and some is copied from Dat Tran's example at
 ## https://github.com/datitran/object_detector_app/blob/master/object_detection_app.py
-
-## but I changed it to make it more understandable to me.
-
 
 # Import packages
 import os
