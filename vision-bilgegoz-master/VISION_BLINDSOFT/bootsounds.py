457b5972343552ccef0ee0ad28424b663b306f72
Ahmet Akif Kaya <ahmetakifkaya11@gmail.com>
2020-02-23 23:16:18 +0300

    Update bootsounds.py

--- vision-bilgegoz-master/VISION_BLINDSOFT/bootsounds.py
+++ vision-bilgegoz-master/VISION_BLINDSOFT/bootsounds.py
@@ -6,6 +6,12 @@
 # This program uses aplay function to play various souneffects of the device on boot.
 # The audio files are for explaining the device to the blind user.
 
+# This code is specifically written for the Vision-Bilgegoz project by Ahmet Akif Kaya
+# and all rights of this program is owned by Ahmet Akif Kaya under MIT open source license. 
+# You are automatically accepting the terms of the MIT license by using this code in any way.
+# The terms and conditions about this project's MIT license can be found the the file named
+# "LICENSE" in the root directory of this repository.
+
 import os
 
 lang = "tr"
